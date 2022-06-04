1db649c151a9b251c86f3bf51a9e54cc960eed9b
Antonio Silva <antoniopsilva083@gmail.com>
2022-06-03 23:53:38 -0300

    feat: create styles sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +11,15 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src='https://github.com/oantoniosilva.png' alt='Avatar do usuário' />
+
         <strong>Antonio Silva</strong>
         <span>Web Development</span>
       </div>
 
       <footer>
         <a href='#'>
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
