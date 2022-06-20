998acf24d66069c9a8c15d14651f757942e2ff11
Antonio <antoniopsilva083@gmail.com>
2022-06-20 00:00:08 -0300

    feat: create like comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,18 @@
 import { Avatar } from './Avatar';
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
+import { useState } from 'react';
+import { set } from 'date-fns/esm';
 
 export function Comment({ content, onDeleteComment }) {
+  	const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -28,9 +36,10 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          {/* <button onClick={() => setLikeCount(likeCount + 1)}> */}
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
